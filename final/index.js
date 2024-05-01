800e7af5f8125d14f351778fa561ce03ea12395e
seasons986 <annalin12345@gmail.com>
2024-05-01 13:33:54 -0400

    makin words

--- final/index.js
+++ final/index.js
@@ -22,18 +22,53 @@ window.addEventListener("load", () => {
 
   const container = document.getElementById("container");
 
-  // for (let i = 0)
+  // MANY YEARS LATER as he faced the firing squad, Colonel Aureliano
+  // Buendía was to remember that distant afternoon when his father took him to
+  // discover ice
   function writeSentence() {
     let sentence = "";
     sentence += times[getRandomInt(0,times.length)];
-    sentence += " as he ";
+    sentence += " as ";
+    sentence += people[getRandomInt(0,people.length)];
+    sentence += " ";
     sentence += verbs[getRandomInt(0,verbs.length)];
     sentence += " the ";
     sentence += nouns[getRandomInt(0,nouns.length)];
     sentence += ", ";
     sentence += people[getRandomInt(0,people.length)];
+    sentence += " ";
+    sentence += verbs[getRandomInt(0,verbs.length)];
+    sentence += " that ";
+    sentence += times[getRandomInt(0,times.length)];
+    sentence += " when ";
+    sentence += people[getRandomInt(0,people.length)];
+    sentence += " ";
+    sentence += verbs[getRandomInt(0,verbs.length)];
+    sentence += " to ";
+    sentence += verbs[getRandomInt(0,verbs.length)];
+    sentence += " ";
+    sentence += nouns[getRandomInt(0,nouns.length)];
     return sentence;
   }
 
-  console.log(writeSentence())
+  function makeP(sentence) {
+    const p = document.createElement("p");
+    p.innerHTML = sentence;
+    container.appendChild(p);
+  }
+
+  function makeA(sentence) {
+    const a = document.createElement("a");
+    a.innerHTML = sentence;
+    a.href = ".";
+    container.appendChild(a);
+  }
+
+  for (let i = 0; i < getRandomInt(1,6); i++) {
+    makeP(writeSentence());
+  }
+  for (let i = 0; i < getRandomInt(1,6); i++) {
+    makeA(writeSentence());
+  }
+
 });
